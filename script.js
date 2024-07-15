524e569b04e03b8045feb07bf5857692edb07aa7
Omkar Dalvi <dalviomkar0901@gmail.com>
2024-07-15 11:39:13 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const disp_cards = document.getElementById('cards')
+const sum = document.getElementById('sum')
+const res = document.getElementById('result')
+
+let card_arr = []
+let s = 0
+let gotblackjack = false
+let isAlive = false
+val_arr = [2,3,4,5,6,7,8,9,10,10,10,10,11]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function start(){
+    res.textContent = "You don't have a Blackjack, yet!"
+    isAlive = true
+    s = 0
+    card_arr = []
+    for(i = 0 ; i < 2; i++){
+        drawcard()
+    }
+}
+function drawcard(){
+    if(!isAlive || gotblackjack){
+        return
+    }
+    index = getRandomInt(13)
+    card = val_arr[index]
+    card_arr.push(card)
+    console.log(card_arr)
+    s += card
+    disp_cards.textContent = "Cards: "
+    for(j = 0; j < card_arr.length;  j++){
+        disp_cards.textContent += card_arr[j] + " "
+    }
+    
+    sum.textContent = "Sum: " + s
+
+    if(s === 21){
+        res.textContent = "You have BlackJack, you Won"
+        gotblackjack = true
+    }
+    if(s > 21){
+        res.textContent = "You lost!"
+        isAlive = false
+    }
+}
